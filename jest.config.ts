6ee70a7be84ad5f0ffef76e04cafddce1cd6bd40
Ihsan Duru <duruihsan@gmail.com>
2025-11-10 01:40:00 +0000

    FAIL  src/__tests__/Login.test.tsx   ● Test suite failed to run
        Jes

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transformIgnorePatterns: [
-    '/node_modules/(?!lucide-react)/',
+    '/node_modules/(?!(lucide-react)/)',
   ],
 };
 
